74d54eab4f95e2b1b0de76327ca7ff15644f6aa1
Minigrim0 <grimauflorent@gmail.com>
2021-04-08 19:45:48 +0200

    [Add] ctf admin

--- ctf/admin.py
+++ ctf/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+import ctf.models as models
+
+
 # Register your models here.
+class CTFAdmin(admin.ModelAdmin):
+    list_display = ("name", "start_date", "end_date", "website")
+    ordering = ("id", "name")
+    search_fields = ("name", )
+
+
+admin.site.register(models.CTF, CTFAdmin)
