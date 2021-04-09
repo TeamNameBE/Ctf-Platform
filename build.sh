140b8c55e6c12015f27ea542b6540649cdd3d78d
Minigrim0 <grimauflorent@gmail.com>
2021-04-09 17:54:01 +0200

    [Fix] use noinput

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 git pull
 
 docker-compose up -d --build
-docker-compose exec web python3 manage.py migrate
-docker-compose exec web python3 manage.py collectstatic
+docker-compose exec web python3 manage.py migrate --noinput
+docker-compose exec web python3 manage.py collectstatic --noinput
 docker-compose restart nginx  # Sometimes the connection between nginx and web fails
