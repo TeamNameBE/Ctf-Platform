b73a2e02ea19f269ce83bb0da64313270331c019
Minigrim0 <grimauflorent@gmail.com>
2021-04-09 17:24:41 +0200

    [Fix] don't crash if user not authenticated

--- ctf/templatetags/gravatar.py
+++ ctf/templatetags/gravatar.py
@@ -6,7 +6,9 @@ register = template.Library()
 
 @register.simple_tag
 def gravatar(user):
-    mail = user.email.lower().encode("utf8")
+    mail = "".encode("utf8")
+    if user.is_authenticated:
+        mail = user.email.lower().encode("utf8")
     gravatar_url = "https://www.gravatar.com/avatar/" + hashlib.md5(mail).hexdigest() + "?d=wavatar"
 
     return gravatar_url
