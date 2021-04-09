8a1808574f17b77edf008c4b7b8ff7cecd2c42d4
Minigrim0 <grimauflorent@gmail.com>
2021-04-09 16:31:04 +0200

    [Add] uploaded files admin

--- ctf/admin.py
+++ ctf/admin.py
@@ -7,21 +7,27 @@ import ctf.models as models
 class CTFAdmin(admin.ModelAdmin):
     list_display = ("name", "start_date", "end_date", "website")
     ordering = ("id", "name")
-    search_fields = ("name", )
+    search_fields = ("name",)
 
 
 class ChallengeAdmin(admin.ModelAdmin):
     list_display = ("name", "points", "validated")
     ordering = ("id", "name")
-    search_fields = ("name", )
+    search_fields = ("name",)
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("name", )
+    list_display = ("name",)
     ordering = ("id", "name")
-    search_fields = ("name", )
+    search_fields = ("name",)
+
+
+class ChallengeFileAdmin(admin.ModelAdmin):
+    list_display = ("file",)
+    ordering = ("id",)
 
 
 admin.site.register(models.CTF, CTFAdmin)
 admin.site.register(models.Challenge, ChallengeAdmin)
 admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.ChallengeFile, ChallengeFileAdmin)
