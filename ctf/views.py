0100f186c709bdb43b6b301342a44189efa72456
Minigrim0 <grimauflorent@gmail.com>
2021-04-09 14:23:03 +0200

    [Add] challenge validation

--- ctf/views.py
+++ ctf/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, reverse
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
 from ctf.models import CTF, Challenge
 
@@ -9,21 +11,28 @@ def challenges(request, ctf_id):
         "page_title": "Challenges",
         "challenges": Challenge.objects.filter(ctf__id=ctf_id),
         "users": User.objects.all(),
-        "ctf": CTF.objects.get(id=ctf_id)
+        "ctf": get_object_or_404(CTF, id=ctf_id),
     }
     return render(request, "challenges.html", context)
 
 
 def calendar(request):
-    context = {
-        "page_title": "Calendar"
-    }
+    context = {"page_title": "Calendar"}
     return render(request, "calendar.html", context)
 
 
 def home(request):
-    context = {
-        "page_title": "Dashboard",
-        "ctfs": CTF.objects.all()
-    }
+    context = {"page_title": "Dashboard", "ctfs": CTF.objects.all()}
     return render(request, "home.html", context)
+
+
+def validate_chall(request, ctf_id, chall_id):
+    challenge = get_object_or_404(Challenge, id=chall_id)
+    challenge.validated = True
+    challenge.save()
+    return HttpResponseRedirect(reverse("chal", kwargs={"ctf_id": ctf_id}))
+
+
+def assign_user(self, ctf_id, chall_id):
+    # TODO
+    return HttpResponseRedirect(reverse("chal", kwargs={"ctf_id": ctf_id}))
