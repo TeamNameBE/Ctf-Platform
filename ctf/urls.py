162b95f13ed6a16379f7d15d5f8386af8ce177c3
Minigrim0 <grimauflorent@gmail.com>
2021-04-09 14:23:17 +0200

    [Upd] urls

--- ctf/urls.py
+++ ctf/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name='dashboard'),
+    path("", views.home, name="dashboard"),
     path("<int:ctf_id>/challenges", views.challenges, name="chal"),
-    path("calendar", views.calendar, name='calendar'),
+    path("<int:ctf_id>/challenges/<int:chall_id>/", views.assign_user, name="assignUser"),
+    path("<int:ctf_id>/challenges/<int:chall_id>/validate", views.validate_chall, name="validate_chall"),
+    path("calendar", views.calendar, name="calendar"),
 ]
