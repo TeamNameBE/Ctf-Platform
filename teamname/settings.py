e3c27663f5a46f72c5cb51a92dab00ccd28d9bc8
Morti <qstevens@hotmail.be>
2021-04-13 14:50:24 +0200

    [ADD] django_extensions

--- teamname/settings.py
+++ teamname/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "theme",
     "tailwind",
     "colorfield",
+    'django_extensions',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
