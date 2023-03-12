b865f67eabb72d5799a34b10071be9b961983b3b
Minigrim0 <grimauflorent@gmail.com>
2023-03-12 17:00:34 +0100

    [Upd] Build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 git pull
 
-docker-compose up -d --build
-docker-compose exec web python3 manage.py migrate --noinput
-docker-compose exec web python3 manage.py collectstatic --noinput
-docker-compose restart nginx  # Sometimes the connection between nginx and web fails
+docker-compose -f docker/docker-compose.yml up -d --build
+docker-compose -f docker/docker-compose.yml exec web python3 manage.py migrate --noinput
+docker-compose -f docker/docker-compose.yml exec web python3 manage.py collectstatic --noinput
+docker-compose -f docker/docker-compose.yml restart nginx  # Sometimes the connection between nginx and web fails
