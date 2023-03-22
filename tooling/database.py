582c5bc970dc615f2f99f3db6ff1d1bb46d6856a
Minigrim0 <grimauflorent@gmail.com>
2023-03-22 11:58:15 +0100

    [Upd] Log database

--- tooling/database.py
+++ tooling/database.py
@@ -1,4 +1,11 @@
+import logging
 import redis
 
+logger = logging.getLogger("[Tooling/database]")
 
-conn = redis.Redis(host="localhost", port=6379, db=0)
+try:
+    logger.info("Connecting to Redis...")
+    conn = redis.Redis(host="localhost", port=6379, db=0)
+    logger.info("Connected to Redis")
+except Exception as e:
+    logger.error(f"Error connecting to Redis: {e}")
